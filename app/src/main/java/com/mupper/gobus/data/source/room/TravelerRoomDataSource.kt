8fa618dd4698310653ce48adbf742501e0aad176
Jesus Daniel Medina Cruz <jesus.medina@insulet.com>
2020-02-22 19:59:26 -0800

    TravelerRoomDataSource 100% covered

--- app/src/main/java/com/mupper/gobus/data/source/room/TravelerRoomDataSource.kt
+++ app/src/main/java/com/mupper/gobus/data/source/room/TravelerRoomDataSource.kt
@@ -5,6 +5,8 @@ import com.mupper.gobus.data.database.GobusDatabase
 import com.mupper.gobus.data.mapper.toDomainTraveler
 import com.mupper.gobus.data.mapper.toRoomTraveler
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import com.mupper.domain.traveler.Traveler as DomainTraveler
 
@@ -16,20 +18,22 @@ class TravelerRoomDataSource(db: GobusDatabase) : TravelerLocalDataSource {
             travelerDao.travelerCount()
         }
 
-    override suspend fun insertTraveler(travelingPath: String, traveler: DomainTraveler) =
-        withContext(Dispatchers.IO) {
+    override suspend fun insertTraveler(travelingPath: String, traveler: DomainTraveler) {
+        GlobalScope.launch(Dispatchers.IO) {
             travelerDao.insertTraveler(traveler.toRoomTraveler(travelingPath))
         }
+    }
 
     override suspend fun findTravelerByEmail(email: String): DomainTraveler? =
         withContext(Dispatchers.IO) {
             travelerDao.findTravelerByEmail(email)?.toDomainTraveler()
         }
 
-    override suspend fun shareActualLocation(traveler: DomainTraveler) =
-        withContext(Dispatchers.IO) {
+    override suspend fun shareActualLocation(traveler: DomainTraveler) {
+        GlobalScope.launch(Dispatchers.IO) {
             val (email, currentPosition) = traveler
             val (latitude, longitude) = currentPosition
             travelerDao.updateTraveler(email, latitude, longitude)
         }
+    }
 }
